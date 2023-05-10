6914306e9733165d905cd90d0078bd7471c88740
Wesley Alvarenga <wsalvarengadev@gmail.com>
2023-05-09 23:20:21 -0300

    Game, repository, service, controller

---
+++ src/main/java/com/wsdev/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.wsdev.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wsdev.dslist.dto.GameMinDTO;
+import com.wsdev.dslist.entities.Game;
+import com.wsdev.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll(){
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+}
